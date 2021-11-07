41ab12ce1d17998f7c86ea1e731e17caab1a3a94
Michael Lesniak <mail@mlesniak.com>
2021-11-07 20:26:12 +0100

    Draw single pixels onto the screen

--- core/src/com/mlesniak/playground/Main.kt
+++ core/src/com/mlesniak/playground/Main.kt
@@ -4,15 +4,30 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx.app
 import com.badlogic.gdx.Gdx.input
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Pixmap
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
+import java.util.*
 
 // TODO(mlesniak) use camera for resolution independent viewport.
 class Main : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
+    private lateinit var pixmap: Pixmap
+    private lateinit var texture: Texture
 
     override fun create() {
         batch = SpriteBatch()
+        pixmap = Pixmap(800, 600, Pixmap.Format.RGB888)
+
+        // Randomize
+        val rand = Random()
+        for (x in 0..pixmap.width) {
+            for (y in 0..pixmap.height) {
+                pixmap.drawPixel(x, y, rand.nextInt())
+            }
+        }
+        texture = Texture(pixmap)
     }
 
     override fun render() {
@@ -20,6 +35,7 @@ class Main : ApplicationAdapter() {
 
         ScreenUtils.clear(24 / 255f, 24 / 255f, 24 / 255f, 1f)
         batch.begin()
+        batch.draw(texture, 0f, 0f)
         batch.end()
     }
 
