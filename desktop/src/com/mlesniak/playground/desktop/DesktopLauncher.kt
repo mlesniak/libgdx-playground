03331ad5d0c92c5219002692f4f7af01317ba844
Michael Lesniak <mail@mlesniak.com>
2021-11-07 20:16:30 +0100

    Upgrade to lwjgl3 and use -XstartOnFirstThread to prevent startup flickering

--- desktop/src/com/mlesniak/playground/desktop/DesktopLauncher.kt
+++ desktop/src/com/mlesniak/playground/desktop/DesktopLauncher.kt
@@ -1,15 +1,15 @@
 package com.mlesniak.playground.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.mlesniak.playground.Main
 
 class DesktopLauncher {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-           val config = LwjglApplicationConfiguration()
-            LwjglApplication(Main(), config)
+           val config = Lwjgl3ApplicationConfiguration()
+            Lwjgl3Application(Main(), config)
         }
     }
 }
